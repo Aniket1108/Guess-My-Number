7bfd0c8a9e2b7b0a2d164ec2fd10b38dd562eca3
Aniket1108 <80505096+Aniket1108@users.noreply.github.com>
2021-03-29 12:07:10 +0530

    Update script.js

--- Guess my Number/script/script.js
+++ Guess my Number/script/script.js
@@ -74,7 +74,7 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    // document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.message').textContent = 'Start guessing...';
     displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
